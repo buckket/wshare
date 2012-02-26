a1acc1b0885c34b8c5b9e3b1b10680a069ca31b2
MrLoom <MrLoom@soeinfachistdas.net>
2012-02-26 08:20:37 +0100

    initial commit

---
+++ public/js/wshare.js
@@ -0,0 +1,89 @@
+$(document).ready(function() {
+    
+ 
+
+    wsinit();
+
+
+    $("#wshare_submit").click(function() {
+
+        //Disable input
+        $('#wshare_url').attr('disabled', true);
+        $('#wshare_submit').button('loading');
+
+        //Getting URL
+        var url = $('input[name=wshare_url]');
+
+        //Checking for valid URL
+        if (! isUrl(url.val())) {
+            $('.control-group').addClass('error');
+            $('#wshare_url').removeAttr('disabled');
+            $('#wshare_submit').button('reset');
+            $('#wshare_submit').removeClass('btn-primary').addClass('btn-danger');
+            return false;
+
+        }
+        else {
+            $('.control-group').removeClass('error');
+            $('#wshare_submit').removeClass('btn-danger').addClass('btn-primary');
+        }
+
+        $.ajax({
+            url: "save",
+            type: "GET",
+            data: encodeURI('url=' + url.val()),
+            success: function (returnData) {
+                //Reenable input
+                $('#wshare_submit').button('reset');
+                $('#wshare_url').removeAttr('disabled');
+            }
+        });
+
+        return false;
+    });
+
+});
+
+wsinit = function() {
+    var ws_uri = "ws://188.192.99.95:3000/ws";
+    if (typeof WebSocket !== "undefined" && WebSocket !== null) {
+        webSocket = typeof WebSocket === "function" ? new WebSocket(ws_uri) : void 0;
+    } else if (typeof MozWebSocket !== "undefined" && MozWebSocket !== null) {
+        webSocket = new MozWebSocket(ws_uri);
+    } else {
+        return;
+    }
+
+    webSocket.onopen = function() {
+        $('#wshare_wsinfo').html("We're live...");
+    }
+
+    webSocket.onclose = function() {
+        $('#wshare_wsinfo').html("Connection lost...");
+    }
+
+    webSocket.onmessage = function(e) {
+        var data = jQuery.parseJSON(e.data);
+        var temp = $('#wshare_thinghidden').clone();
+        temp.hide();
+        temp.removeAttr('id');
+        $('.url', temp).append("<a href='" + data.url + "'>" + data.url + "</a>");
+        $('.subtitle', temp).append(data.subtitle);
+
+        if ($('.thing').length >= 11) {
+            $('.thing:last-child').fadeOut('fast', function() {
+                $('.thing:last-child').remove()
+                temp.insertAfter('#wshare_thinghidden');
+                temp.fadeIn('slow');
+            });   
+        } else {
+            temp.insertAfter('#wshare_thinghidden');
+            temp.fadeIn('slow');
+        }
+    }
+}
+
+function isUrl(s) {
+    var regexp = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+    return regexp.test(s);
+}
---
+++ wshare.pl
@@ -0,0 +1,104 @@
+#!/usr/bin/env perl
+use Mojolicious::Lite;
+use Mojo::ByteStream 'b';
+use Mojo::JSON;
+use utf8;
+
+
+my $clients = {};
+
+get '/' => sub {
+  my $self = shift;
+  
+  $self->render('index');
+};
+
+any '/save' => sub {
+    my $self = shift;
+    my $url = $self->param('url');
+    $url = b($url)->html_escape;
+
+    app->log->debug(sprintf 'Got URL: %s', $url);
+    my $json = Mojo::JSON->new;
+    for (keys %$clients) {
+        $clients->{$_}->send_message(
+            b($json->encode({
+                url => $url,
+                subtitle => 'yet to come',
+            }))->decode('utf-8')->to_string
+        );
+    }
+
+    $self->render(text => "Thanks for $url ");
+};
+
+websocket '/ws' => sub {
+    my $self = shift;
+
+    app->log->debug(sprintf 'WS Client connected: %s', $self->tx);
+    my $id = sprintf '%s', $self->tx;
+    $clients->{$id} = $self->tx;
+
+    $self->on(finish =>
+        sub {
+            app->log->debug('WS Client disconnected');
+            delete $clients->{$id};
+        });
+};
+
+
+app->start;
+__DATA__
+
+@@ index.html.ep
+% layout 'upload', title => 'wShare :: Upload';
+
+
+@@ layouts/upload.html.ep
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><%= title %></title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+
+        <script type="text/javascript" src="js/jquery-1.7.1.js" ></script>
+        <script type="text/javascript" src="js/bootstrap.js" ></script>
+        <script type="text/javascript" src="js/wshare.js"></script>   
+    
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <div class="container">
+            <h1 id="wshare_header">wShare</h1>
+            <div class="row">
+                <div class="span6 offset3" id="wshare_inputcontainer">
+                    <form class="well form-inline" method="post" action="save">
+                        <div class="control-group">
+                            <input class="span4" type="text" name="wshare_url" id="wshare_url" placeholder="http://example.org" autofocus />
+                            <button type="submit" class="btn btn-primary" id="wshare_submit" data-loading-text="Loading">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="row" id="wshare_contentcontainer">
+                <div class="well span8 offset2" id="wshare_things">
+                    <div class="thing" id="wshare_thinghidden">
+                        <p class="url">
+                        </p>
+                        <p class="subtitle"></p>
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="row">
+                <div id="wshare_navigation">
+                    <a href="#">&laquo; Newer</a> <a href="#">Older &raquo;</a>
+                </div>
+            </div>
+            <div class="row">
+                <div id="wshare_wsinfo"></div>
+            </div>
+        </div>
+    </body>
+</html>
